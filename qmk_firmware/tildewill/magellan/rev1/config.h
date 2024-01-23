566b4bd071bf1a83e1b6110d598c4ecf70f49144
Will Read <will.read@gmail.com>
2024-01-23 13:07:16 -0800

    Adds firmware t local repo rather than having it separate

---
+++ qmk_firmware/tildewill/magellan/rev1/config.h
@@ -0,0 +1,28 @@
+// Copyright 2024 Will Read (@tildewill)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+#define AZOTEQ_IQS5XX_TPS65
+#define AZOTEQ_IQS5XX_TAP_ENABLE true
+#define AZOTEQ_IQS5XX_TWO_FINGER_TAP_ENABLE true
+#define AZOTEQ_IQS5XX_PRESS_AND_HOLD_ENABLE true
+#define AZOTEQ_IQS5XX_SWIPE_X_ENABLE true
+#define AZOTEQ_IQS5XX_SWIPE_Y_ENABLE true
+#define AZOTEQ_IQS5XX_ZOOM_ENABLE true
+#define AZOTEQ_IQS5XX_SCROLL_ENABLE true
---
+++ qmk_firmware/tildewill/magellan/rev1/rules.mk
@@ -0,0 +1,4 @@
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = azoteq_iqs5xx # setting found at https://docs.qmk.fm/#/feature_pointing_device?id=azoteq-iqs5xx-trackpad
+
+
